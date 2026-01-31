ef223e75136603d0ecffcfcdf643c7d237ff53c2
Chris Nielsen <chris.rahbek.nielsen@gmail.com>
2026-01-31 20:09:43 +0100

    Adjusted userstruct to use uuid for createdby and updatedby. Go mod adjustments to make uuid direkt instead of indirect - go mod tidy.
    Signed-off-by: Chris Nielsen <chris.rahbek.nielsen@gmail.com>

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -20,8 +20,8 @@ type User struct {
 	LastLogin    time.Time `json:"last_login"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
-	CreatedBy    string    `json:"created_by"`
-	UpdatedBy    string    `json:"updated_by"`
+	CreatedBy    uuid.UUID `json:"created_by"`
+	UpdatedBy    uuid.UUID `json:"updated_by"`
 }
 
 var (
